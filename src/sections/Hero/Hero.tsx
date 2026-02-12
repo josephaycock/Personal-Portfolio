e74026d0fb59cab54de458204196329e819dcd6b
Joseph Aycock <josephfelixaycock@outlook.com>
2026-02-12 13:38:29 -0600

    Simple Changes to the hero section

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -3,7 +3,18 @@ import "./Hero.css"
 function Hero() {
   return (
     <section className="hero">
-      Hero
+      <h1 className="hero-title">
+        Hi, I'm Joseph.
+      </h1>
+      <h3 className="hero-subtitle">
+        Software Engineer | M.S. Computer Science Student at LSU
+      </h3>
+      <p className="hero-paragraph">
+        I focus on full-stack application development and clean system design. I value structure, scalability, and writing maintainable code.
+      </p>
+      <button className="hero-button">
+        About Me
+      </button>
     </section>
   )
 }
